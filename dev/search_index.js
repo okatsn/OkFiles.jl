dba648c23c5c046d0978c6846cf9f38f45f598fc
Documenter.jl <documenter@juliadocs.github.io>
2022-12-15 09:55:01 +0000

    build based on 225db58

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkFiles","category":"page"},{"location":"#OkFiles","page":"Home","title":"OkFiles","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkFiles.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkFiles]","category":"page"}]
+}
