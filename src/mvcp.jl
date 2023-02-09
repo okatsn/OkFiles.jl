a0f50dfb9a146146f9b130b0db7390c21dce0187
okatsn <okatsn@gmail.com>
2023-02-09 17:44:26 +0800

    mv2dir with mkpath kwargs

--- src/mvcp.jl
+++ src/mvcp.jl
@@ -2,7 +2,11 @@
 """
 `mv2dir(item, newdir::AbstractString)` moves `item` to the `newdir` directory, preseving its original name.
 """
-function mv2dir(item, newdir::AbstractString)
+function mv2dir(item, newdir::AbstractString; mkpath=false)
+    if mkpath
+        Base.mkpath(newdir)
+    end
+
     newpath = joinpath(newdir, basename(item))
     mv(item, newpath)
     return newpath
--- test/mv2cp.jl
+++ test/mv2cp.jl
@@ -2,9 +2,47 @@
     using RDatasets,CSV
     iris = RDatasets.dataset("datasets", "iris")
 
-    targetdir = joinpath("data")
+    ## Create source file
+    srcfile = "iris.csv"
+    try
+        CSV.write(srcfile, iris)
+    catch e
+        @warn "Please clean the test folder before test."
+        rethrow(e)
+    end
 
-    mkpath.(targetdir)
+    ## Define target directory
+    targetdir = joinpath("data")
+    # targetdir created first
+    mkpath(targetdir)
+
+
+    targetfile = mv2dir(srcfile, targetdir)
+
+    @test !isfile(srcfile)
+    @test isfile(targetfile)
+    @test basename(targetfile) == basename(srcfile)
+    @test targetfile == joinpath(targetdir, srcfile)
+
+    @test pathnorepeat(targetfile) == joinpath(targetdir, "iris$(OkFiles.serial_number_4d(1)).csv")
+
+    @test isequal(CSV.read(targetfile,DataFrame), iris)
+
+    ## Remove file
+    rm(targetfile)
+    @test !isfile(targetfile) || "File for test not removed correctly"
+
+
+
+end
+
+
+
+@testset "mv2dir with mkpath" begin
+    using RDatasets,CSV
+    iris = RDatasets.dataset("datasets", "iris")
+
+    ## Create source file
     srcfile = "iris.csv"
 
     try
@@ -13,15 +51,27 @@
         @warn "Please clean the test folder before test."
         rethrow(e)
     end
-    targetfile = mv2dir(srcfile, targetdir)
+
+    ## Define target directory
+    targetdir = joinpath("data", "foo", "bar")
+    # targetdir NOT created first
+
+    targetfile = mv2dir(srcfile, targetdir; mkpath=true)
 
     @test !isfile(srcfile)
     @test isfile(targetfile)
     @test basename(targetfile) == basename(srcfile)
+    @test targetfile == joinpath(targetdir, srcfile)
 
     @test pathnorepeat(targetfile) == joinpath(targetdir, "iris$(OkFiles.serial_number_4d(1)).csv")
 
     @test isequal(CSV.read(targetfile,DataFrame), iris)
-    rm(targetfile)
+
+    ## Remove file and folder
+    rm("data"; recursive=true)
     @test !isfile(targetfile) || "File for test not removed correctly"
+    @test !isdir(joinpath("data","foo")) || "Folder for test does not removed correctly"
+
+
+
 end
