4e6cb8ec5d63523821616570a2f632827dd4a4cb
okatsn <okatsn@gmail.com>
2023-02-10 11:30:12 +0800

    Docstring and test

--- src/mvcp.jl
+++ src/mvcp.jl
@@ -21,7 +21,7 @@ end
 
 `mv2dir` takes keyword arguments of `mv`.
 
-# Example
+# Examples
 $mkdirway_brief:
 ```julia
 mv2dir("hello/world/iris.csv", "another/world", mkdirway) # results in "another/world/iris.csv"
@@ -36,14 +36,23 @@ mv2dir("hello/world/iris.csv", "another/world") # results in "another/world/iris
 If `srcfile` is a directory,
 ```julia
 mkpath("another") # noted that "another/world" not created
-mv2dir("hello/world", "another/world") # results in "another/world/iris.csv"
+mv2dir("hello/world", "another") # Move all contents under "world" to the new folder "another", resulting in "another/world/iris.csv"
 ```
 which is equivualent to
 ```julia
-mv2dir("hello/world", "another/world", mkdirway) # results in "another/world/iris.csv"
+mv2dir("hello/world", "another", mkdirway) # results in "another/world/iris.csv"
 ```
 
 Also see the docstring of `mkdirway`.
+
+# Another example
+
+You can apply `f = pathnorepeat` that
+```julia
+mv2dir("hello/world", "another", pathnorepeat) # results in "another/world_0001/iris.csv" if directory "another/world" already exists.
+
+mv2dir("hello/world/iris.csv", "another/world", pathnorepeat) # results in "another/world/iris_0001.csv" if file "another/world/iris.csv" already exists.
+```
 
 """
 function mv2dir(srcfile, newdir::AbstractString, f::Function; kwargs...)
--- test/mv2cp.jl
+++ test/mv2cp.jl
@@ -120,10 +120,13 @@ end
 
 
     ## Test move a directory to another directory
-    targetdir = mv2dir(srcdir, joinpath("another","world"), OkFiles.mkdirway)
+    targetdir = mv2dir(srcdir, joinpath("another","world"), OkFiles.mkdirway; force=true)
     @test isdir(targetdir)
     @test !isfile(srcfile1)
-    @test isfile(joinpath("another", "world","hello", "world", "iris.csv"))
+    targetfile1 = joinpath("another", "world", "hello", "world", "iris.csv")
+    targetfile2 = joinpath("another", "world", "hello", "quakes.csv")
+    @test isfile(targetfile1)
+    @test isfile(targetfile2)
     @test targetdir == joinpath("another", "world","hello")
     @test basename(targetdir) == basename(srcdir)
     # @test targetfile == joinpath(targetdir, basename(srcfile1))
@@ -134,7 +137,59 @@ end
 
     # ## Remove file and folder
     # rm("data"; recursive=true)
-    # @test !isfile(targetfile) || "File for test not removed correctly"
-    # @test !isdir(joinpath("data","foo")) || "Folder for test does not removed correctly"
+    rm("another"; recursive=true)
+    @test all(.!isfile.([targetfile1,targetfile2])) || "File for test not removed correctly"
+    @test !isdir(srcdir) || "Folder for test does not removed correctly"
+
+end
+
+
+@testset "Test apply pathnorepeat" begin
+    using RDatasets,CSV
+    iris = RDatasets.dataset("datasets", "iris")
+
+    ## Create source file
+    srcfile1 = joinpath("hello", "world", "iris.csv")
+    targetfile1 = joinpath("another", "world", "iris.csv")
+
+
+
+    OkFiles.mkdirway.(srcfile1)
+    OkFiles.mkdirway.(targetfile1)
+    @test isdir(dirname(srcfile1))
+    @test isdir(dirname(targetfile1))
+
+    try
+        CSV.write(srcfile1, iris)
+        CSV.write(targetfile1, iris)
+    catch e
+        @warn "Please clean the test folder before test."
+        rethrow(e)
+    end
+
+    ## Test move a directory to another directory
+    # Move "hello/world/*" to another/
+    srcdir = joinpath("hello", "world")
+    targetdir = mv2dir(srcdir, joinpath("another"), OkFiles.pathnorepeat)
+    @test isdir(targetdir)
+    @test !isfile(srcfile1)
+    # "another/world" exists, "pathnorepeat" produce "another/world_0001"
+    targetfile1 = joinpath("another", "world", "iris.csv")
+    targetfile_0001 = joinpath("another", "world_0001", "iris.csv")
+    @test isfile(targetfile1)
+    @test isfile(targetfile_0001)
+    @test targetdir == joinpath("another", "world_0001")
+    @test basename(targetfile_0001) == basename(srcfile1)
+
+    newpath = mv2dir(targetfile1, targetdir, OkFiles.pathnorepeat)
+    @test !isfile(targetfile1)
+    @test isfile(joinpath(targetdir, "iris_0001.csv"))
+    @test newpath == joinpath(targetdir, "iris_0001.csv")
+
+    # ## Remove file and folder
+    rm("another"; recursive=true)
+    rm("hello")
+    @test all(.!isfile.([targetfile1, targetfile_0001])) || "File for test not removed correctly"
+    @test !isdir(srcdir) || "Folder for test does not removed correctly"
 
 end
