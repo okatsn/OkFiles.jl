ae2bb5d3052bf577ab9b0f9de85390dbfc97cfe1
okatsn <okatsn@gmail.com>
2024-05-13 10:13:24 +0000

    `filelistall` for SFTP

--- src/filelistall.jl
+++ src/filelistall.jl
@@ -4,14 +4,9 @@ Similar to `filelist`, `filelistall(dir)` returns all files in the directory (**
 function filelistall(dir)
     wd = walkdir(dir)
     allpaths = String[]
-    for (root, dirs, files) in wd
-        for file in files
-            path_i = joinpath(root, file)
-            push!(allpaths, path_i)
-        end
-    end
+    iteratedirwalked!(wd, allpaths)
     return allpaths
-end 
+end
 
 """
 Similar to `filelist`, `filelistall(expr::Regex, dir)` returns files whose name matches `expr` in the directory (**as well as its subdirectory**) `dir`.
@@ -20,4 +15,29 @@ function filelistall(expr::Regex, dir)
     allpaths = filelistall(dir)
     desired_ind = occursin.(expr, basename.(allpaths))
     return allpaths[desired_ind]
+end
+
+"""
+
+```julia
+SFTPClient.download.(sftp, fpath, downloadDir="") # download the entire folder of root.
+```
+
+"""
+function filelistall(sftp::SFTPClient.SFTP, vararg...)
+    allpaths = String[]
+    subdir = only(vararg)
+    wd = SFTPClient.walkdir(sftp, subdir)
+    iteratedirwalked!(wd, allpaths)
+    return allpaths
+end
+
+
+function iteratedirwalked!(wd, allpaths)
+    for (root, dirs, files) in wd
+        for file in files
+            path_i = joinpath(root, file)
+            push!(allpaths, path_i)
+        end
+    end
 end
